102b80164928c017e0e2dc811797a236a0310967
Alex Avk <iwanov.vany-ivanov@yandex.ru>
2023-09-19 16:10:27 +0300

    setup project and  created Navbar

---
+++ src/components/Logo.jsx
@@ -0,0 +1,30 @@
+import Link from 'next/link';
+import React from 'react';
+import { motion } from 'framer-motion';
+
+const MotionLink = motion(Link);
+
+const Logo = () => {
+  return (
+    <div className="flex items-center justify-center mt-2">
+      <MotionLink
+        href="/"
+        whileHover={{
+          transition: { duration: 1, repeat: Infinity },
+          backgroundColor: [
+            '#121212',
+            'rgba(131,58,180,1)',
+            'rgba(253,29,29,1)',
+            'rgba(252,176,69,1)',
+            'rgba(131,58,180,1)',
+            '#121212',
+          ],
+        }}
+        className="w-16 h-16 bg-dark text-light flex justify-center items-center rounded-full text-2xl font-bold">
+        CB
+      </MotionLink>
+    </div>
+  );
+};
+
+export default Logo;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,87 @@
+import Link from 'next/link';
+import React from 'react';
+import Logo from './Logo';
+import { useRouter } from 'next/router';
+import {
+  TwitterIcon,
+  DribbbleIcon,
+  GithubIcon,
+  LinkedInIcon,
+  PinterestIcon,
+} from './Icons';
+import { motion } from 'framer-motion';
+
+const MotionLink = motion(Link);
+
+const CustonLink = ({ href, title, className = '' }) => {
+  const router = useRouter();
+
+  return (
+    <Link href={href} className={`${className} relative group`}>
+      {title}
+      <span
+        className={`h-[1px] inline-block bg-dark absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300 ${
+          router.asPath === href ? 'w-full' : 'w-0'
+        }`}>
+        &nbsp;
+      </span>
+    </Link>
+  );
+};
+
+const NavBar = () => {
+  return (
+    <header className="w-full px-32 py-8 font-medium flex items-center justify-between">
+      <nav>
+        <CustonLink href="/" title="Home" className="mr-4" />
+        <CustonLink href="/about" title="About" className="mx-4" />
+        <CustonLink href="/projects" title="Projects" className="mx-4" />
+        <CustonLink href="/articles" title="Articles" className="ml-4" />
+      </nav>
+
+      <nav className="flex items-center justify-center flex-wrap">
+        <MotionLink
+          href="/"
+          whileHover={{ y: -2 }}
+          whileTap={{ scale: 0.9 }}
+          className="w-6 mr-3">
+          <TwitterIcon />
+        </MotionLink>
+        <MotionLink
+          href="/"
+          whileHover={{ y: -2 }}
+          whileTap={{ scale: 0.9 }}
+          className="w-6 mx-3">
+          <GithubIcon />
+        </MotionLink>
+        <MotionLink
+          href="/"
+          whileHover={{ y: -2 }}
+          whileTap={{ scale: 0.9 }}
+          className="w-6 mx-3">
+          <LinkedInIcon />
+        </MotionLink>
+        <MotionLink
+          href="/"
+          whileHover={{ y: -2 }}
+          whileTap={{ scale: 0.9 }}
+          className="w-6 mx-3">
+          <PinterestIcon />
+        </MotionLink>
+        <MotionLink
+          href="/"
+          whileHover={{ y: -2 }}
+          whileTap={{ scale: 0.9 }}
+          className="w-6 ml-3">
+          <DribbbleIcon />
+        </MotionLink>
+      </nav>
+
+      <div className="absolute left-[50%] top-2 translate-x-[-50%]">
+        <Logo />
+      </div>
+    </header>
+  );
+};
+
+export default NavBar;
