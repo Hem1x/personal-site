3dc01efeacc14a5034038500be3ca23f3217ff6b
Alex Avk <iwanov.vany-ivanov@yandex.ru>
2023-09-20 17:56:15 +0300

    Article page is completed

--- src/pages/articles.jsx
+++ src/pages/articles.jsx
@@ -1,7 +1,178 @@
-import React from 'react';
+import AnimatedText from '@/components/AnimatedText';
+import Layout from '@/components/Layout';
+import Head from 'next/head';
+import Image from 'next/image';
+import Link from 'next/link';
+import React, { useRef } from 'react';
+import featuresArticle1 from '../../public/images/articles/pagination component in reactjs.jpg';
+import featuresArticle2 from '../../public/images/articles/create loading screen in react js.jpg';
+import { motion, useMotionValue } from 'framer-motion';
+import article1 from '../../public/images/articles/create modal component in react using react portals.png';
+
+const FramerImage = motion(Image);
+
+const MovingImg = ({ title, img, link }) => {
+  const x = useMotionValue(0);
+  const y = useMotionValue(0);
+  const imgRef = useRef(null);
+
+  const handleMouse = (e) => {
+    imgRef.current.style.display = 'inline-block';
+    x.set(event.pageX);
+    y.set(-10);
+  };
+
+  const handleMouseLeave = (e) => {
+    imgRef.current.style.display = 'none';
+  };
+
+  return (
+    <Link
+      href={link}
+      target="_blank"
+      onMouseMove={handleMouse}
+      onMouseLeave={handleMouseLeave}>
+      <h2 className="capitalize text-xl font-semibold hover:underline">
+        {title}
+      </h2>
+      <FramerImage
+        style={{ x, y }}
+        initial={{ opacity: 0 }}
+        exit={{ opacity: 0 }}
+        whileInView={{
+          opacity: 1,
+          transition: {
+            duration: 0.2,
+          },
+        }}
+        ref={imgRef}
+        src={img}
+        alt={title}
+        className="w-96 h-auto hidden absolute rounded-lg z-10"
+      />
+    </Link>
+  );
+};
+
+const Article = ({ img, title, date, link }) => {
+  return (
+    <motion.li
+      initial={{ y: 200 }}
+      whileInView={{
+        y: 0,
+        transition: {
+          duration: 0.5,
+          ease: 'easeInOut',
+        },
+      }}
+      viewport={{ once: true }}
+      className="relative w-full p-4 py-6 my-4 rounded-xl flex items-center justify-between bg-light text-dark first:mt-0 border border-solid border-dark border-r-4 border-b-4">
+      <MovingImg title={title} img={img} link={link} />
+      <span className="text-primary font-semibold pl-4">{date}</span>
+    </motion.li>
+  );
+};
+
+const FeatuedArticle = ({ img, title, time, summary, link }) => {
+  return (
+    <li className="relative col-span-1 w-full p-4 bg-light border border-solid border-dark rounded-2xl">
+      <div className="absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2rem] rounded-br-3xl bg-dark" />
+      <Link
+        href={link}
+        target="_blank"
+        className="w-full inline-block cursor-pointer overflow-hidden rounded-lg">
+        <FramerImage
+          whileHover={{ scale: 1.05 }}
+          transition={{ duration: 0.2 }}
+          src={img}
+          alt={title}
+          className="w-full h-auto rounded-lg"
+        />
+      </Link>
+      <Link href={link} target="_blank">
+        <h2 className="capitalize text-2xl font-bold my-2 mt-4 hover:underline">
+          {title}
+        </h2>
+      </Link>
+      <p className="text-sm mb-2">{summary}</p>
+      <span className="text-primary font-semibold">{time} min read</span>
+    </li>
+  );
+};
 
 const articles = () => {
-  return <div>articles</div>;
+  return (
+    <>
+      <Head>
+        <title>Alex | Статьи</title>
+        <meta name="description" content="any description" />
+      </Head>
+      <main className="w-full mb-16 flex flex-col items-center justify-center overflow-hidden">
+        <Layout className="pt-16">
+          <AnimatedText
+            text="Words Can Change The World!"
+            className="mb-16"
+          />
+          <ul className="grid grid-cols-2 gap-16">
+            <FeatuedArticle
+              img={featuresArticle1}
+              title={`Build A Custom Pagination Component In Reactjs From Scratch`}
+              time={9}
+              summary={`Learn how to build a custom pagination component in ReactJS from scratch. 
+            Follow this step-by-step guide to integrate Pagination component in your ReactJS project.`}
+              link={'/'}
+            />
+
+            <FeatuedArticle
+              img={featuresArticle2}
+              title={`Creating Stunning Loading Screens In React: Build 3 Types Of Loading Screens`}
+              time={10}
+              summary={`Learn how to create stunning loading screens in React with 3 different methods. 
+              Discover how to use React-Loading, React-Lottie & build a custom loading screen. 
+              Improve the user experience.`}
+              link={'/'}
+            />
+          </ul>
+          <h2 className="font-bold text-4xl w-full text-center my-16 mt-22">
+            All Articles
+          </h2>
+          <ul>
+            <Article
+              img={article1}
+              title={`Form Validation In Reactjs: Build A Reusable Custom Hook For Inputs And Error Handling`}
+              date="March 22, 2023"
+              link="/"
+            />
+            <Article
+              img={article1}
+              title={`Form Validation In Reactjs: Build A Reusable Custom Hook For Inputs And Error Handling`}
+              date="March 22, 2023"
+              link="/"
+            />
+            <Article
+              img={article1}
+              title={`Form Validation In Reactjs: Build A Reusable Custom Hook For Inputs And Error Handling`}
+              date="March 22, 2023"
+              link="/"
+            />
+            <Article
+              img={article1}
+              title={`Form Validation In Reactjs: Build A Reusable Custom Hook For Inputs And Error Handling`}
+              date="March 22, 2023"
+              link="/"
+            />
+            <Article
+              img={article1}
+              title={`Form Validation In Reactjs: Build A Reusable Custom Hook For Inputs And Error Handling`}
+              date="March 22, 2023"
+              link="/"
+            />
+          </ul>
+        </Layout>
+      </main>
+      {/* <HireMe /> */}
+    </>
+  );
 };
 
 export default articles;
--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -8,6 +8,9 @@ import Link from 'next/link';
 import React from 'react';
 import project1 from '../../public/images/projects/crypto-screener-cover-image.jpg';
 import project2 from '../../public/images/projects/portfolio-cover-image.jpg';
+import { motion } from 'framer-motion';
+
+const FramerImage = motion(Image);
 
 const FeaturedProject = ({
   type,
@@ -24,7 +27,13 @@ const FeaturedProject = ({
         target="_blank"
         className="w-1/2 cursor-pointer overflow-hidden rounded-lg">
         <div className="absolute top-0 -right-4 -z-10 w-[101%] h-[103%] rounded-[2.5rem] rounded-br-3xl bg-dark" />
-        <Image src={img} alt={title} className="w-full h-auto" />
+        <FramerImage
+          whileHover={{ scale: 1.05 }}
+          transition={{ duration: 0.2 }}
+          src={img}
+          alt={title}
+          className="w-full h-auto"
+        />
       </Link>
 
       <div className="w-1/2 flex flex-col items-start justify-between pl-6">
@@ -57,12 +66,18 @@ const FeaturedProject = ({
 const Project = ({ title, type, img, link, githubLink }) => {
   return (
     <article className="w-full flex flex-col items-center justify-center rounded-2xl border border-solid border-dark bg-light p-6 relative">
+      <div className="absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2rem] rounded-br-3xl bg-dark" />
       <Link
         href={link}
         target="_blank"
         className="w-full cursor-pointer overflow-hidden rounded-lg">
-        <div className="absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2rem] rounded-br-3xl bg-dark" />
-        <Image src={img} alt={title} className="w-full h-auto" />
+        <FramerImage
+          whileHover={{ scale: 1.05 }}
+          transition={{ duration: 0.2 }}
+          src={img}
+          alt={title}
+          className="w-full h-auto"
+        />
       </Link>
 
       <div className="w-full flex flex-col items-start justify-between mt-4">
@@ -168,7 +183,7 @@ const projects = () => {
           </div>
         </Layout>
       </main>
-      {/* <HireMe /> */}
+      <HireMe />
     </>
   );
 };
